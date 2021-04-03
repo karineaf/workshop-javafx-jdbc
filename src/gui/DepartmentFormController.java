db41f29e1cdb071c592ec11adad048d2c8f2de3b
karine <karineantunesfarias@gmail.com>
2021-04-03 11:35:41 -0300

    passando um objeto de department para o departmentform view

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -9,8 +9,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.entities.Department;
 
 public class DepartmentFormController implements Initializable {
+
+    private Department department;
 
     @FXML
     private TextField txtId;
@@ -26,6 +29,10 @@ public class DepartmentFormController implements Initializable {
 
     @FXML
     private Button btCancel;
+
+    public void setDepartment(Department entity){
+        this.department = entity;
+    }
 
     @FXML
     public void onBtSaveAction() {
@@ -45,5 +52,14 @@ public class DepartmentFormController implements Initializable {
     private void initializeNodes() {
         Constraints.setTextFieldInteger(txtId);
         Constraints.setTextFieldMaxLength(txtName, 30);
+    }
+
+    public void updateFormData(){
+
+        if(department == null)
+            throw new IllegalStateException("Entity was null");
+
+        txtId.setText(String.valueOf(department.getId()));
+        txtName.setText(department.getName());
     }
 }
