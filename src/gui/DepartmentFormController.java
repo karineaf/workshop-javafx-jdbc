7aec18bb27e2a33527f24b4218d7d14953f8f53b
karine <karineantunesfarias@gmail.com>
2021-04-03 12:36:45 -0300

    utilizacao de exception p/ validacao de campos no departmentform

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,9 +1,7 @@
 package gui;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.util.*;
 
 import db.DbException;
 import gui.listeners.DataChangeListener;
@@ -18,6 +16,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
@@ -68,6 +67,8 @@ public class DepartmentFormController implements Initializable {
             Utils.currentStage(event).close();
         } catch (DbException e){
             Alerts.showAlert("Error saving object", null,  e.getMessage(), Alert.AlertType.ERROR);
+        } catch (ValidationException e){
+            setErrorMessages(e.getErrors());
         }
     }
 
@@ -79,8 +80,19 @@ public class DepartmentFormController implements Initializable {
 
     private Department getFormData() {
         Department dep = new Department();
+
+        ValidationException exception = new ValidationException("Validation error");
+
         dep.setId(Utils.tryParseToInt(txtId.getText()));
+
+        if(txtName.getText() == null || txtName.getText().trim().equals(""))
+            exception.addError("name", "Field can't be empty");
+
         dep.setName(txtName.getText());
+
+        if(exception.getErrors().size() > 0)
+            throw exception;
+
         return dep;
     }
 
@@ -106,5 +118,11 @@ public class DepartmentFormController implements Initializable {
 
         txtId.setText(String.valueOf(department.getId()));
         txtName.setText(department.getName());
+    }
+
+    public void setErrorMessages(Map<String, String> errors){
+        Set<String> fields = errors.keySet();
+        if(fields.contains("name"))
+            labelErrorName.setText(errors.get("name"));
     }
 }
